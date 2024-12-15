736c10fd2ee721c3cb5bfc0b7b78d25c16905ff7
Maria <marriiamnii@gmail.com>
2024-12-16 00:00:12 +0800

    added image reading via ImageReader

--- symbols_extractor.py
+++ symbols_extractor.py
@@ -1,11 +1,15 @@
-from PIL import Image
+
 import pytesseract
+from tesseract_reader import ImageReader
+from tesseract_reader.bbox.bbox import BBox
 
-
-class symbols_extractor():
+class SymbolsExtractor:
+    
     @staticmethod
     def take_symbols_and_bboxes(filename, lang):
-        string = pytesseract.image_to_boxes(Image.open(filename), lang)
+        image_reader = ImageReader()
+        string = pytesseract.image_to_boxes(image_reader.read(filename), lang)
         string = string.split('\n')
+
         return string
 
