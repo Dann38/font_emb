ed82ae913740b9e3a8337b8218f5a20cc984a4d6
Maria <marriiamnii@gmail.com>
2024-12-18 11:47:35 +0800

    deleted comments

--- char_matrix_extractor.py
+++ char_matrix_extractor.py
@@ -1,24 +1,10 @@
 from tesseract_reader.bbox.bbox import BBox
-from typing import List, Tuple
+from typing import List
 import numpy as np
-from pytesseract import pytesseract
-from tesseract_reader import TesseractReader, TesseractReaderConfig, ImageReader
 
 
 class CharMatrixExtractor():
-    #[BBox(x = 2, y = 4, w = 5, h = 3), BBox(x = 5, y = 3, w = 3, h = 3)]
-    
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    # [P P P P P P P P P P P P P]
-    
+
     @staticmethod
     def matrix_from_bboxes(img: np.array, list_bboxes: List['BBox'], list_char: List['str']):
         char_image_list = []
