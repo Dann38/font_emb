375a6ed4cee33ed479182c52643510243ab2ae85
Maria <marriiamnii@gmail.com>
2025-02-17 10:31:20 +0800

    fixed

--- classifier/font_generator.py
+++ classifier/font_generator.py
@@ -6,6 +6,7 @@ import tkcap
 from PIL import Image
 import os
 from string_generator import StringGenerator
+import itertools
 
 root = tkinter.Tk()
 root.title("Fonts")
@@ -20,46 +21,41 @@ underline = [True, False]
 overstrike = [True, False]
 background = ['yellow', 'green', 'red', 'blue', None]
 
+combinations = itertools.product(weight, slant, underline, overstrike, background, fonts)
+
+
+def generate_file_name(name, w, s, u, o, b, i):
+
+    file_name = f'training_images/{name}_{w}_{s}_'
+    file_name += 'u_' if underline else 'nu_'
+    file_name += 'o_' if overstrike else 'no_'
+    file_name += b + '_' if b else ''
+
+    return file_name + str(i) + '_eng' + '.png'
+
+
 i = 0
-for name in fonts:
-    for w in weight:
-        for s in slant:
-            for u in underline:
-                for o in overstrike:
-                    for b in background:
+for combo in combinations:
+    w, s, u, o, b, name = combo
+    text = StringGenerator.text_generator('eng')
+    font = tkinter.font.Font(family=name, size=18, weight=w, slant=s, underline=u, overstrike=o)
+    label = tkinter.Label(root, text=text, font=font, background=b, foreground='black')
+    label.pack(pady=10)
 
-                        text = StringGenerator.text_generator('eng')
+    root.update()  # обновляется окно чтобы отобразить текст
 
-                        font = tkinter.font.Font(family=name, size=18, weight=w, slant=s, underline=u, overstrike=o)
-                        label = tkinter.Label(root, text=text, font=font, background=b, foreground='black')
-                        label.pack(pady=10)
+    file_name = generate_file_name(name, w, s, u, o, b, i)
 
-                        root.update() # обновляется окно чтобы отобразить текст
+    cap = tkcap.CAP(root)
+    cap.capture(file_name)
 
-                        cap = tkcap.CAP(root)
-                        file_name = f'training_images/{name}_{w}_{s}_'
-                        if underline:
-                            file_name += 'u_'
-                        else:
-                            file_name += 'nu_'
+    image = Image.open(file_name)
+    image_crop = image.crop((0, 28, 120, 83))
+    os.remove(file_name)
+    image_crop.save(file_name, quality=95)
 
-                        if overstrike:
-                            file_name += 'o_'
-                        else:
-                            file_name += 'no_'
+    label.destroy()  # удаляет текст
 
-                        if b:
-                            file_name += b + '_'
-
-                        cap.capture(file_name + str(i) + '_eng' + '.png')
-
-                        image = Image.open(file_name + str(i) + '_eng' + '.png')
-                        image_crop = image.crop((0, 28, 120, 83))
-                        os.remove(file_name + str(i) + '_eng' + '.png')
-                        image_crop.save(file_name + str(i) + '_eng' + '.png', quality=95)
-
-                        label.destroy() # удаляет текст
-
-                        i += 1
+    i += 1
 
 
