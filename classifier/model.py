5c6c934e0809176a52ad55e17a284d12fae136e4
Maria <marriiamnii@gmail.com>
2025-03-10 00:00:56 +0800

    trained model

--- classifier/model.py
+++ classifier/model.py
@@ -1,14 +1,12 @@
+import cv2
+import numpy as np
 import torch
 import torch.nn as nn
-import torchvision
-import torch.nn.functional as F
-
-
-num_epochs = 5
-num_classes = 20
-batch_size = 100
-learning_rate = 0.01
-DATA_PATH = ''
+from torch import optim
+from torch.utils.data import Dataset, DataLoader
+from pytorch_dataset import CharImageDataset, label_to_vec, image_to_gray
+from pil_font_generator import FontGenerator
+from PIL import Image
 
 
 class CharCNNClassifier(nn.Module):
@@ -23,22 +21,106 @@ class CharCNNClassifier(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Полносвязные слои
-        self.fc1 = nn.Linear(32 * 10 * 7, 128)  # После пулинга размер изображения уменьшается
-        self.fc2 = nn.Linear(128, 3)  # Выходной вектор длины 3
+        self.fc1 = nn.Linear(32 * 15 * 60, 128)  # Обновлено
+        self.fc2 = nn.Linear(128, 1)  # Выходной вектор длины 1
 
         # Функция активации
         self.relu = nn.ReLU()
 
     def forward(self, x):
         # Применяем свертки и пулинг
-        x = self.pool(self.relu(self.conv1(x)))  # Размер: (batch_size, 16, 20, 15) <- размер уменьшился в 2 раза
-        x = self.pool(self.relu(self.conv2(x)))  # Размер: (batch_size, 32, 10, 7) <- размер уменьшился в 2 раза
+        x = self.pool(self.relu(self.conv1(x)))  # (batch_size, 16, 30, 120)
+        x = self.pool(self.relu(self.conv2(x)))  # (batch_size, 32, 15, 60)
 
         # Выравниваем тензор для полносвязного слоя
-        x = x.view(x.size(0), -1)  # Размер: (batch_size, 32 * 10 * 7)
+        x = x.view(x.size(0), -1)  # (batch_size, 32 * 15 * 60)
 
         # Применяем полносвязные слои
-        x = self.relu(self.fc1(x))  # Размер: (batch_size, 128)
-        x = self.fc2(x)  # Размер: (batch_size, 3)
+        x = self.relu(self.fc1(x))  # (batch_size, 128)
+        x = self.fc2(x)  # (batch_size, 1)
 
         return x
+
+
+dataset = CharImageDataset("dataset/", transform=image_to_gray, target_transform=label_to_vec)
+
+num_epochs = 20
+
+# model = CharCNNClassifier()
+#
+# example_batch_x = torch.stack([dataset[i][0] for i in range(2)])  # (2, 1, 60, 240)
+# example_batch_y = torch.stack([dataset[i][1] for i in range(2)])  # (2, 1)
+#
+# # проверка входных данных
+# print("example_batch_x shape:", example_batch_x.shape)  # (2, 1, 60, 240)
+# print("example_batch_y shape:", example_batch_y.shape)  # (2, 1)
+#
+# output = model(example_batch_x)
+#
+# # проверка выходных данных
+# print("Output shape:", output.shape)  # (2, 1)
+# print("Output:", output)
+# print("Labels:", example_batch_y)
+
+
+model = CharCNNClassifier()
+criterion = nn.BCEWithLogitsLoss() # функция потерь
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+train = DataLoader(dataset, batch_size=128, shuffle=True)
+for epoch in range(num_epochs):
+    model.train()  # Переводим модель в режим обучения
+    running_loss = 0.0
+
+    for i, (inputs, targets) in enumerate(train):
+        # Обнуляем градиенты
+        optimizer.zero_grad()
+
+        # Прямой проход
+        outputs = model(inputs)
+
+        # Вычисление потерь
+        loss = criterion(outputs, targets)
+
+        # Обратное распространение и обновление весов
+        loss.backward()
+        optimizer.step()
+
+        # Суммируем потери для вывода
+        running_loss += loss.item()
+
+        # Выводим статистику каждые 10 батчей
+        if i % 10 == 9:
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Batch [{i + 1}/{len(train)}], Loss: {running_loss / 10:.4f}")
+            running_loss = 0.0
+
+
+# Проверка работы модели
+
+def classifier(model, char):
+    gray_image = image_to_gray(char)
+    data = gray_image.unsqueeze(0)
+    rez = model(data)
+    return torch.mean(rez, 0)
+
+
+def interpretation_class(v):
+    if v < 0.5:
+        print("Шрифты разные")
+    else:
+        print("Шрифты одинаковые")
+
+
+font_generator = FontGenerator()
+font_generator.generate_images(1, answer=1, style=True)
+
+image_path = 'dataset/image_1.png'
+image = Image.open(image_path)
+
+image_array = np.array(image)
+
+# проверка размерности
+# print("image_array shape:", image_array.shape)  # (60, 240, 3)
+
+v = classifier(model, image_array)
+print(v)
+interpretation_class(v)
